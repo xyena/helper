0b5cf116117b178b5f0f0f0e8e73291a5902cab9
xyena <spotcheckd20@gmail.com>
2024-02-25 18:33:39 -0600

    Directionless commit

--- !js/dexload.js
+++ !js/dexload.js
@@ -1,14 +1,26 @@
 spitTxt("https://xyena.github.io/helper/allpkmn.txt");
 
+
+
+
+//https://stackoverflow.com/questions/2998784/how-to-output-numbers-with-leading-zeros-in-javascript/2998822#2998822
+function pad(num, size) {
+    var s = "000000000" + num;
+    return s.substr(s.length-size);
+}
+
 async function spitTxt(file) {
 	let obj = await fetch(file);
 	let txt = await obj.text(); //string; proven by console.log(typeof txt);
+	document.getElementById("dexol").innerHTML = "Loading...";
+	var completedHTML = "";
 	var looping = true;
-	var i=0;
-	for (i=0;looping==true;i++) { //TODO: don't use .split or .match on txt because it creates 1025 pointers in an array. Find a better way to do this?
+	for (i=0;looping==true;i++) { //avoid using .split or .match on txt because it creates 1025 pointers in an array.
 		var tmp = txt.indexOf("\n");
 		if (tmp==-1){tmp=txt.length;looping=false;} //we are at the end if no newlines are detected, or something else is wrong. Prepare to exit.
-		document.getElementById("dexol").innerHTML += "<li><p class=\"dexentry\" id=\"de" + (i+1) + "\">" + txt.substring(0,tmp) + "</p></li>";
+		completedHTML += "<li><p class=\"dexentry\" id=\"de" + (i+1) + "\"><span class=\"numbah\">" + pad(i+1,4) + ":</span> " + txt.substring(0,tmp) + "</p></li>";
 		txt = txt.substring(tmp+1); //trims off what we just read
 	}
+	document.getElementById("dexol").innerHTML = completedHTML;
 }
+
