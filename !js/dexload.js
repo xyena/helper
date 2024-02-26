e07f701d02550a1087817728605fd34053a8f116
xyena <spotcheckd20@gmail.com>
2024-02-26 00:22:25 -0600

    Greatly reduced list down to avoid excessive calls to a certain website.

--- !js/dexload.js
+++ !js/dexload.js
@@ -9,17 +9,60 @@ function pad(num, size) {
     return s.substr(s.length-size);
 }
 
+function slapImg(pkmn, frm) {
+	var upoint = "<img src=\"https://play.pokemonshowdown.com/sprites/gen5";
+	pkmn = pkmn.toLowerCase();
+	frm = frm.toLowerCase();
+	
+	upoint += "/" + pkmn + ".png\" />";
+	if (frm="") {return upoint}
+	else {
+		switch (frm) {
+			case "female":
+				return upoint + "-f.png\" />";
+			default:
+				return upoint + "-" + frm + ".png\" />";
+		}
+	}
+}
+
 async function spitTxt(file) {
 	let obj = await fetch(file);
 	let txt = await obj.text(); //string; proven by console.log(typeof txt);
 	document.getElementById("dexol").innerHTML = "Loading...";
+	
+	//TESTING ONLY
+	txt="Bulbasaur\nIvysaur\nVenusaur\nCharmander\nCharmeleon\nCharizard\nSquirtle\nWartortle\nBlastoise";
+	
 	var completedHTML = "";
 	var looping = true;
-	for (i=0;looping==true;i++) { //avoid using .split or .match on txt because it creates 1025 pointers in an array.
-		var tmp = txt.indexOf("\n");
-		if (tmp==-1){tmp=txt.length;looping=false;} //we are at the end if no newlines are detected, or something else is wrong. Prepare to exit.
-		completedHTML += "<li><p class=\"dexentry\" id=\"de" + (i+1) + "\"><span class=\"numbah\">" + pad(i+1,4) + ":</span> " + txt.substring(0,tmp) + "</p></li>";
-		txt = txt.substring(tmp+1); //trims off what we just read
+	var i=0; //since i controls the dex number of the output, we want to be specific about when we increment. Declare outside
+	
+	while (looping==true) { //avoid using .split or .match on txt because it creates 1025 pointers in an array.
+		var position = txt.indexOf("\n");
+		if (position==-1){position=txt.length;looping=false;} //we are at the end if no newlines are detected. Prep to exit at the end of this loop
+		if (txt.substring(0,2)!="//") { //if not a comment line
+			if (txt.substring(0,position).includes(",")==false) {
+				completedHTML += "<li><p class=\"dexentry\" id=\"de" + (i+1) + "\">";
+				completedHTML += slapImg(txt.substring(0,position),"");
+				completedHTML += "<span class=\"numbah\">" + pad(i+1,4) + ":</span> ";
+				completedHTML += txt.substring(0,position) + "</p></li>";
+				i++;
+			}
+			else { //it does contain a comma
+				var line = txt.substring(0,position).split(","); //split the current line
+				var j=0;
+				completedHTML += "<li><p class=\"dexentry\" id=\"de" + (i+1) + "\"><span class=\"numbah\">" + pad(i+1,4) + ":</span> " + line[0] + "</p></li>"; //do the first one normally
+				
+				for (j=1;j<line.length;j++) {
+					line[j];
+					completedHTML += "<li><p class=\"dexentry\" id=\"de" + (i+1+"&#") + (97+(j-1)) + "\;\"><span class=\"numbah\">" + pad(i+1,4) + "&#" + (97+(j-1)) + "\;:</span> " + line[0] + line[j] + "</p></li>";
+				}
+				i++;
+			}
+		}
+		txt = txt.substring(position+1); //trims off what we just read
+		console.log(i + ": " + position);
 	}
 	document.getElementById("dexol").innerHTML = completedHTML;
 }
--- !js/site.js
+++ !js/site.js
@@ -1,10 +1,4 @@
 	
-	var upoint = "https://play.pokemonshowdown.com/sprites/"
-	var spritetype =	["gen5", //or xyani, or gen5-shiny, or xyani-shiny
-						".png"]; //change to .gif for xyani and xyani-shiny
-	//we want to load sprites by default, not models. that increases load times
-	
-	
 	document.getElementById("ftr").innerHTML="Pokémon and All Respective Names are Trademark &amp; &copy; of Nintendo 1996&ndash;";
 	document.getElementById("ftr").innerHTML+=new Date().getFullYear();
 	
